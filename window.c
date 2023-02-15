f4eeb652dca3a7ef4810b22c80745be63f59b139
CheemsBread <dogecheemsbread@gmail.com>
2023-02-15 00:17:35 -0500

    Uploaded the main code file.

---
+++ window.c
@@ -0,0 +1,48 @@
+//to run use "gcc window.c -o window -lX11 -g".
+
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    Display *display = XOpenDisplay(NULL);
+
+    if (display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        exit(1);
+    }
+
+    int screen_num = DefaultScreen(display);
+    Window root_window = RootWindow(display, screen_num);
+    unsigned long border_color = BlackPixel(display, screen_num);
+    unsigned long background_color = WhitePixel(display, screen_num);
+    Window window = XCreateSimpleWindow(display, root_window, 0, 0, 200, 200, 2, border_color, background_color);
+
+    XStoreName(display, window, "My Window Title");
+
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    XMapWindow(display, window);
+
+    XEvent event;
+    while (1) {
+        XNextEvent(display, &event);
+        if (event.type == Expose) {
+            XFontStruct *font_info;
+            GC gc = DefaultGC(display, screen_num);
+            font_info = XLoadQueryFont(display, "fixed");
+            if (font_info == NULL) {
+                fprintf(stderr, "Error loading font\n");
+                exit(1);
+            }
+            XSetFont(display, gc, font_info->fid);
+            char *msg = "Hello, World!";
+            XDrawString(display, window, gc, 50, 50, msg, strlen(msg));
+        }
+    }
+
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
+
+    return 0;
+}
